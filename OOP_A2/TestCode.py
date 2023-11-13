137b14c6fd1d7c468f1f0a4fc94e2c59d1a4fc72
LiamHenkeUni <henld003@mymail.unisa.edu.au>
2023-11-13 21:53:15 +1030

    First Commit, folder and file structure setup.

---
+++ OOP_A2/TestCode.py
@@ -0,0 +1,8 @@
+'''
+File: TestCode.py
+Description: This file is for the test code for the 2nd assignment in Object-Oriented Programming.
+Author: Liam Henke
+StudentID: 110377752
+EmailID: henld003
+This is my own work as defined by the University's Academic Misconduct Policy.
+'''
