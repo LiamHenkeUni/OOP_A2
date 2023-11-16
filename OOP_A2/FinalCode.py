023c8822c2f3fea6777443181dab652274804612
LiamHenkeUni <henld003@mymail.unisa.edu.au>
2023-11-16 17:30:48 +1030

    Finished all the __init__.

--- OOP_A2/FinalCode.py
+++ OOP_A2/FinalCode.py
@@ -8,33 +8,57 @@ This is my own work as defined by the University's Academic Misconduct Policy.
 '''
 
 class Laboratory:
-    def __init__(self, potions, herbs, catalysts):
-        pass
+    def __init__(self, potions: list, herbs: list, catalysts: list):
+        self.__potions = potions
+        self.__herbs = herbs
+        self.__catalysts = catalysts
 
 class Alchemist:
-    def __init__(self, attack, strength, defense, magic, ranged, necromancy, laboratory, recipes):
-        pass
-
-class Potion:
-    def __init__(self, name, stat, boost):
-        pass
-
-class SuperPotion(Potion):
-    def __init__(self, name, stat, boost, herb, catalyst):
-        pass
-
-class ExtremePotion(Potion):
-    def __init__(self, name, stat, boost, reagent, potion):
-        pass
+    def __init__(self, attack: int, strength: int, defense: int, magic: int, ranged: int, necromancy: int, laboratory: Laboratory, recipes: dict):
+        self.__attack = attack
+        self.__strength = strength
+        self.__defense = defense
+        self.__magic = magic
+        self.__ranged = ranged
+        self.__necromancy = necromancy
+        self.__laboratory = laboratory
+        self.__recipes = recipes
 
 class Reagent:
-    def __init__(self, name, potency):
-        pass
+    def __init__(self, name: str, potency: float):
+        self.__name = name
+        self.__potency = potency
 
 class Herb(Reagent):
-    def __init__(self, name, potency, grimy):
-        pass
+    def __init__(self, name: str, potency: float, grimy: True):
+        self.__name = name
+        self.__potency = potency
+        self.__grimy = grimy
 
 class Catalyst(Reagent):
-    def __init__(self, name, potency, quality):
-        pass
+    def __init__(self, name: str, potency: float, quality: float):
+        self.__name = name
+        self.__potency = potency
+        self.__quality = quality
+
+class Potion:
+    def __init__(self, name: str, stat: str, boost: float):
+        self.__name = name
+        self.__stat = stat
+        self.__boost = boost
+
+class SuperPotion(Potion):
+    def __init__(self, name: str, stat: str, boost: str, herb: Herb, catalyst: Catalyst):
+        self.__name = name
+        self.__stat = stat
+        self.__boost = boost
+        self.__herb = herb
+        self.__catalyst = catalyst
+
+class ExtremePotion(Potion):
+    def __init__(self, name: str, stat: str, boost: str, reagent: Reagent, potion: Potion):
+        self.__name = name
+        self.__stat = stat
+        self.__boost = boost
+        self.__reagent = reagent
+        self.__potion = potion
